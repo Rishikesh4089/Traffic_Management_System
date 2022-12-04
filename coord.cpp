ca83fcc1273bae95e1617f2f2160dbadaac7a6bb
Rishikesh Prabhu <110054654+Rishikesh4089@users.noreply.github.com>
2022-12-04 14:12:15 +0530

    Create coord.cpp

---
+++ coord.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void coord(int x, int y)
+{
+    for(int j=0 ; j<y; j++)
+    {
+        cout<<endl;
+    }
+    for(int i=0; i<x; i++)
+    {
+        cout<<" ";
+    }
+}
